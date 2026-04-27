93d371eecbb0aae48abe4a5ab0f514f47d66cbf1
IsaMtz269 <mtzisabela8@gmail.com>
2026-04-27 07:10:26 -0600

    Actualizacion del Readme, agrega APIs POST para Citas, Fechas Prohibidas y Reseñas con validaciones de negocio

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,12 @@ mongoose.connect(MONGO_URI)
 
 const Usuario = require('./models/Usuario');
 const Negocio = require('./models/Negocio');
- 
+const Servicio = require('./models/Servicio');
+const Post = require('./models/Post');
+const Cita = require('./models/Cita');
+const FechaProhibida = require('./models/FechaProhibida');
+
+//Registrar un cliente 
 app.post('/api/register', async (req, res) => {
     
     const { nombre, apellido, email, password, fechNacimiento } = req.body;
@@ -44,6 +49,7 @@ app.post('/api/register', async (req, res) => {
     res.status(201).json({ message: 'Usuario registrado exitosamente' });
 });
 
+//Login
 app.post('/api/login', async (req, res) => {
     const { email, password } = req.body;
     if(!email || !password) {
@@ -76,7 +82,7 @@ app.post('/api/login', async (req, res) => {
     });
 });
 
-
+//Agregar un negocio
 app.post('/api/negocios', async (req, res) => {
     const { 
         nombre, tipo, eslogan, descripcion, celular, ubicacion, 
@@ -110,6 +116,234 @@ app.post('/api/negocios', async (req, res) => {
         res.status(500).json({ message: 'Error al crear el negocio', error: error.message });
     }
 });
+
+//Agregar servicio a un negocio
+app.post('/api/servicios', async (req, res) => {
+    try {
+        const nuevoServicio = new Servicio(req.body);
+        await nuevoServicio.save();
+        res.status(201).json({ message: 'Servicio creado', servicio: nuevoServicio });
+    } catch (error) {
+        res.status(400).json({ message: 'Error al crear servicio', error: error.message });
+    }
+});
+
+// Eliminar servicio
+app.delete('/api/servicios/:id', async (req, res) => {
+    try {
+        await Servicio.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Servicio eliminado correctamente' });
+    } catch (error) {
+        res.status(400).json({ message: 'Error al eliminar', error: error.message });
+    }
+});
+
+// Agregar post a un negocio
+app.post('/api/posts', async (req, res) => {
+    try {
+        const nuevoPost = new Post(req.body);
+        await nuevoPost.save();
+        res.status(201).json({ message: 'Post creado', post: nuevoPost });
+    } catch (error) {
+        res.status(400).json({ message: 'Error al crear post', error: error.message });
+    }
+});
+
+// Eliminar post
+app.delete('/api/posts/:id', async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Post eliminado correctamente' });
+    } catch (error) {
+        res.status(400).json({ message: 'Error al eliminar post', error: error.message });
+    }
+});
+
+// Crear empleado
+app.post('/api/empleados', async (req, res) => {
+    try {
+        const { 
+            nombre, apellido, email, password, fechNacimiento, 
+            id_negocio, servicio_empl, horario_dia, horario_inicio, horario_final 
+        } = req.body;
+
+        if (!nombre || !apellido || !email || !password || !fechNacimiento || !id_negocio) {
+            return res.status(400).json({ message: 'Faltan campos obligatorios' });
+        }
+
+        const negocio = await Negocio.findById(id_negocio);
+        if (!negocio) {
+            return res.status(404).json({ message: 'Negocio no encontrado' });
+        }
+
+        const cantidadServicios = await Servicio.countDocuments({ id_negocio: id_negocio });
+        
+        if (cantidadServicios === 0) {
+            return res.status(400).json({ 
+                message: 'No puedes agregar empleados sin antes haber registrado al menos un servicio en tu negocio.' 
+            });
+        }
+
+        const nuevoEmpleado = new Usuario({
+            nombre, 
+            apellido, 
+            email, 
+            password,
+            fechNacimiento, 
+            trabaja_en: id_negocio,
+            roles: 'empleado',
+            is_empleado: true,
+            servicio_empl,
+            horario_dia,
+            horario_inicio,
+            horario_final
+        });
+
+        await nuevoEmpleado.save();
+        res.status(201).json({ message: 'Empleado creado exitosamente', empleado: nuevoEmpleado });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear empleado', error: error.message });
+    }
+});
+
+// Bloquear una fecha (Día Festivo, Vacaciones, etc.)
+app.post('/api/fechas-prohibidas', async (req, res) => {
+    try {
+        const { id_negocio, fecha_f, razon_f } = req.body;
+
+        if (!id_negocio || !fecha_f || !razon_f) {
+            return res.status(400).json({ message: 'Faltan campos obligatorios para bloquear la fecha' });
+        }
+
+        const negocio = await Negocio.findById(id_negocio);
+        if (!negocio) {
+            return res.status(404).json({ message: 'Negocio no encontrado' });
+        }
+
+        const nuevaFechaProhibida = new FechaProhibida({
+            id_negocio,
+            fecha_f,
+            razon_f
+        });
+
+        await nuevaFechaProhibida.save();
+
+        res.status(201).json({ 
+            message: 'Día cerrado exitosamente. Nadie podrá agendar citas en esta fecha.', 
+            fecha_bloqueada: nuevaFechaProhibida 
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error al bloquear la fecha', error: error.message });
+    }
+});
+
+// Agendar una nueva cita
+app.post('/api/citas', async (req, res) => {
+    try {
+        const { 
+            id_cliente, id_negocio, id_servicio, 
+            fecha, hora, precio_final, anticipo_pagado 
+        } = req.body;
+
+        if (!id_cliente || !id_negocio || !id_servicio || !fecha || !hora) {
+            return res.status(400).json({ message: 'Faltan campos obligatorios para agendar la cita' });
+        }
+
+        const fechaHoraCita = new Date(`${fecha}T${hora}:00`); 
+        const ahora = new Date();
+        
+        const diferenciaMilisegundos = fechaHoraCita - ahora;
+        const diferenciaHoras = diferenciaMilisegundos / (1000 * 60 * 60);
+
+        if (diferenciaHoras < 48) {
+            return res.status(400).json({ 
+                message: 'Por políticas del negocio, las citas deben agendarse con al menos 48 horas de anticipación.' 
+            });
+        }
+
+        const fechaBloqueada = await FechaProhibida.findOne({ 
+            id_negocio: id_negocio, 
+            fecha_f: new Date(fecha) 
+        });
+
+        if (fechaBloqueada) {
+            return res.status(400).json({ 
+                message: `No es posible agendar en esta fecha: ${fechaBloqueada.razon_f}` 
+            });
+        }
+
+        const nuevaCita = new Cita({
+            id_cliente,
+            id_negocio,
+            id_servicio,
+            fecha,
+            hora,
+            precio_final,
+            anticipo_pagado
+        });
+
+        await nuevaCita.save();
+
+        res.status(201).json({ 
+            message: 'Tu cita ha sido enviada y está en revisión', 
+            cita: nuevaCita 
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agendar la cita', error: error.message });
+    }
+});
+
+// Agregar una reseña a una cita completada
+app.post('/api/citas/:id_cita/resena', async (req, res) => {
+    try {
+        const { id_cita } = req.params; // Sacamos el ID de la cita de la URL
+        const { id_cliente, review_stars, review_texto } = req.body;
+
+        if (!id_cliente || !review_stars || !review_texto) {
+            return res.status(400).json({ message: 'Faltan campos obligatorios para la reseña' });
+        }
+
+        if (review_stars < 1 || review_stars > 5) {
+            return res.status(400).json({ message: 'La calificación debe estar entre 1 y 5 estrellas' });
+        }
+
+        const cita = await Cita.findById(id_cita);
+        if (!cita) {
+            return res.status(404).json({ message: 'La cita no existe' });
+        }
+
+        if (cita.id_cliente.toString() !== id_cliente.toString()) {
+            return res.status(403).json({ message: 'No tienes permiso para reseñar una cita de otra persona' });
+        }
+
+        if (cita.estado !== 'completada') {
+            return res.status(400).json({ 
+                message: `No puedes reseñar esta cita porque su estado actual es: ${cita.estado}. Solo se permiten reseñas en citas completadas.` 
+            });
+        }
+
+        if (cita.review_done === true) {
+            return res.status(400).json({ message: 'Ya has dejado una reseña para esta cita anteriormente.' });
+        }
+
+        cita.review_stars = review_stars;
+        cita.review_texto = review_texto;
+        cita.review_done = true;
+
+        await cita.save();
+
+        res.status(200).json({ 
+            message: '¡Gracias por tu reseña! Se ha guardado exitosamente.', 
+            citaActualizada: cita 
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error al procesar la reseña', error: error.message });
+    }
+});
+
 
 app.get('/api/', (req, res) => {
     res.json({ message: 'API funcionando' });
